a1b81513afadcbb2a3e12d4ea484b92a3b30ee7b
Caleb Bennett <bennettcm4@mymail.vcu.edu>
2020-08-30 09:41:57 -0400

    Setting up homework

--- week2/homework.py
+++ week2/homework.py
@@ -1,5 +1,21 @@
-# My portfolio
-This is my portfolio
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-## This is my work
-awdawdawd
+def squared_threes():
+    return value = []
+    # YOUR CODE GOES HERE
+
+    # END SHOULDNT GO BEYOND HERE
+    return return_value
+
+if __name__ == "__main__":
+    for x in squared_threes():
+        print(x)
+    
+    # should print out all numbers between 0 and 99 (inclusive)
+    # that are evenly divisible by 3, then squared
+    # e.g
+    # 9
+    # 36
+    # 81
+    # ..... etc
