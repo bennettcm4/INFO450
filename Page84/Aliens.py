be8eb3a4e22ac687b7699fe60427997bf50802d0
Caleb Bennett <bennettcm4@mymail.vcu.edu>
2020-09-03 20:29:23 -0400

    After class

--- Page84/Aliens.py
+++ Page84/Aliens.py
@@ -1,2 +1,28 @@
 # Caleb
-# This will be page 84 assignment
+# This will be page 84 assignment.
+
+alien_color = 'red'
+
+# green 5 
+# yellow 10
+# red 15
+
+def get_points(killed_color):
+    earned_points = 0
+
+    if killed_color == 'green':
+        earned_points = 5
+    elif killed_color == 'yellow':
+        earned_points = 10
+    elif killed_color == 'red':
+        earned_points = 15
+    elif killed_color == 'blue':
+        earned_points = 3
+
+    return earned_points
+
+if __name__ == "__main__":
+    assert get_points('red') == 15
+    assert get_points('yellow') == 10
+    assert get_points('green') == 5
+    assert get_points('green') == 3
---
+++ week3/check_users.py
@@ -0,0 +1,6 @@
+# Page 89
+# Complete 5-10 on page 89 of your book by completing the following program:
+
+def check_users(current_users, new_users):
+    pass
+    # YOUR CODE HERE
