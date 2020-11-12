a75ef1cff7329b9df8af34560e53d3069d3f4cd9
Caleb Bennett <bennettcm4@mymail.vcu.edu>
2020-11-12 17:59:51 -0500

    hw

---
+++ final/client/client.py
@@ -0,0 +1,10 @@
+"""
+Must accept user input representing a 'name'
+Must make HTTP GET call to server in the form of:
+http://localhost:5000/hello/{name}
+"""
+
+import requests
+
+r = requests.get("http://localhost:5000/hello{name}")
+print(r.text)
