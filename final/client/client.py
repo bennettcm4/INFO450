29c102d3494a34b04f544612105c7645785d3888
Caleb Bennett <bennettcm4@mymail.vcu.edu>
2020-11-19 10:36:52 -0500

    hw 1.0

--- final/client/client.py
+++ final/client/client.py
@@ -1,9 +1,3 @@
-"""
-Must accept user input representing a 'name'
-Must make HTTP GET call to server in the form of:
-http://localhost:5000/hello/{name}
-"""
-
 import requests
 
 print("Please enter your name: ")
@@ -11,3 +5,4 @@ name = input()
 url = f"http://localhost:5000/hello/{name}"
 response = requests.get(url)
 print(response.text)
+
--- final/server/server.py
+++ final/server/server.py
@@ -1,13 +1,6 @@
-"""
-Must accept a path /hello with a parameter for 'name'
-Return value must be a dictionary with the key 'hello' and the value of the name url parameter
-Server should listen on port 5000
-"""
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello_name(name):
     return {"Hello": name}
-
